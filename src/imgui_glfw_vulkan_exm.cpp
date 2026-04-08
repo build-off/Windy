b70e6486ef41fdbcaf32f9d259547de90865d404
SMKM <msantiagovargas2@gmail.com>
2026-04-08 09:41:19 -0600

    initial setup and tests for jolt

--- src/imgui_glfw_vulkan_exm.cpp
+++ src/imgui_glfw_vulkan_exm.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <cstdarg>
 
 #include <common/TracySystem.hpp>
 #include <tracy/Tracy.hpp>
@@ -13,6 +15,46 @@
 #include <GLFW/glfw3.h>
 
 #include <Jolt/Jolt.h>
+
+#include <Jolt/RegisterTypes.h>
+#include <Jolt/Core/Factory.h>
+#include <Jolt/Core/Core.h>
+#include <Jolt/Core/TempAllocator.h>
+#include <Jolt/Core/JobSystemThreadPool.h>
+#include <Jolt/Physics/PhysicsSettings.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+#include <Jolt/Physics/Collision/Shape/BoxShape.h>
+#include <Jolt/Physics/Collision/Shape/SphereShape.h>
+#include <Jolt/Physics/Body/BodyCreationSettings.h>
+#include <Jolt/Physics/Body/BodyActivationListener.h>
+#include <Jolt/Physics/Collision/ObjectLayer.h>
+
+JPH_SUPPRESS_WARNINGS
+
+static void TraceImpl(const char* inFMT, ...) {
+  va_list list;
+  va_start(list, inFMT);
+  char buffer[1024];
+  vsnprintf(buffer, sizeof(buffer), inFMT, list);
+  va_end(list);
+
+  std::cout << buffer << std::endl;
+}
+
+#ifdef JPH_ENABLE_ASSERTS
+static bool AssertFailedImpl(const char* inExpression, const char* inMessage,
+                             const char* inFile, uint inLine) {
+  std::cout << inFile << ":" << inLine << ": (" << inExpression << ") "
+            << (inMessage != nullptr ? inMessage : "") << std::endl;
+  return true;
+}
+#endif // JPH_ENABLE_ASSERTS
+
+namespace Layers {
+static constexpr JPH::ObjectLayer NON_MOVING = 0;
+static constexpr JPH::ObjectLayer MOVING = 1;
+static constexpr JPH::ObjectLayer NUM_LAYERS = 2;
+} // namespace Layers
 
 // Volk headers
 #ifdef IMGUI_IMPL_VULKAN_USE_VOLK
