3351cd85d310b9ece05dab6846a03e0348b3b24c
SMKM <msantiagovargas2@gmail.com>
2026-04-14 15:05:25 -0600

    reading the contents and returning the bytes

--- src/core/entry_point.cpp
+++ src/core/entry_point.cpp
@@ -39,6 +39,11 @@ static std::vector<char> read_file(const std::string& filename) {
   if (!file.is_open()) {
     throw std::runtime_error("failed to open file!");
   }
+  std::vector<char> buffer(file.tellg());
+  file.seekg(0, std::ios::beg);
+  file.read(buffer.data(), static_cast<std::streamsize>(buffer.size()));
+  file.close();
+  return buffer;
 }
 
 class Renderer {
