f6f7fbe07bab1209d78336e2abd8b66f9e46768e
SMKM <msantiagovargas2@gmail.com>
2026-04-17 10:41:08 -0600

    handling minimization

--- src/core/entry_point.cpp
+++ src/core/entry_point.cpp
@@ -701,6 +701,14 @@ class Renderer {
   }
 
   void recreateSwapChain() {
+    int width = 0, height = 0;
+    glfwGetFramebufferSize(window, &width, &height);
+
+    while (width == 0 || height == 0) {
+      glfwGetFramebufferSize(window, &width, &height);
+      glfwWaitEvents();
+    }
+
     device.waitIdle();
     cleanupSwapChain();
     createSwapChain();
