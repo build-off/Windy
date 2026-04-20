942001c46d2efd25c766dcc412cdcf3f74c16be5
SMKM <msantiagovargas2@gmail.com>
2026-04-19 19:27:54 -0600

    create and copy image data into the buffer

--- src/core/entry_point.cpp
+++ src/core/entry_point.cpp
@@ -955,6 +955,18 @@ class Renderer {
                                 &texChannels, STBI_rgb_alpha);
     vk::DeviceSize imageSize = texWidth * texHeight * 4;
     if (!pixels) throw std::runtime_error("failed to load texture image!");
+
+    vk::raii::Buffer stagingBuffer({});
+    vk::raii::DeviceMemory stagingBufferMemory({});
+
+    createBuffer(imageSize, vk::BufferUsageFlagBits::eTransferSrc,
+                 vk::MemoryPropertyFlagBits::eHostVisible |
+                     vk::MemoryPropertyFlagBits::eHostCoherent,
+                 stagingBuffer, stagingBufferMemory);
+    void* data = stagingBufferMemory.mapMemory(0, imageSize);
+    memcpy(data, pixels, imageSize);
+    stagingBufferMemory.unmapMemory();
+    stbi_image_free(pixels);
   }
 
   void initvulkan() {
