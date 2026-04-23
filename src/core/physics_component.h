343e8eccbe5305c1ec075629ccb3f25429fc0243
SMKM <msantiagovargas2@gmail.com>
2026-04-23 09:43:32 -0600

    testing events

---
+++ src/core/physics_component.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "component.h"
+#include "event.h"
+#include "event_system.h"
+namespace Windy {
+
+class PhysicsComponent : public Component, public EventListener {
+public:
+  // grab the event_system and register a new listener
+  void init() override { get_event_system().add_listener(this); }
+
+  // remove the listener when the component is destroyed
+  ~PhysicsComponent() override { get_event_system().remove_listener(this); };
+
+private:
+  EventSystem& get_event_system() {};
+};
+
+} // namespace Windy
