c270287b2ff94be3176d6a72008ca2dd72af7327
SMKM <msantiagovargas2@gmail.com>
2026-04-23 09:31:03 -0600

    dispatched for the events

---
+++ src/core/event_system.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "event.h"
+#include <algorithm>
+#include <vector>
+
+namespace Windy {
+class EventSystem {
+public:
+  void add_listener(EventListener *listener) { listeners.push_back(listener); }
+  bool remove_listener(EventListener *listener) {
+    auto it = std::find(listeners.begin(), listeners.end(), listener);
+    if (it != listeners.end()) {
+      listeners.erase(it);
+      return true;
+    }
+    return false;
+  }
+
+  void dispatch_event(const Event& event) {
+    for (auto listener : listeners) {
+      listener->OnEvent(event);
+    }
+  }
+
+private:
+  std::vector<EventListener *> listeners;
+};
+
+} // namespace Windy
