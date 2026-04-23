1910074ec2c000a1c59c867a5c792f08cb1152db
SMKM <msantiagovargas2@gmail.com>
2026-04-23 09:43:26 -0600

    lifecycle components

--- src/core/component.h
+++ src/core/component.h
@@ -4,18 +4,54 @@ namespace Windy {
 
 class Entity;
 class Component {
+public:
+  enum class State {
+    Uninitialized,
+    Initializing,
+    Active,
+    Destroying,
+    Destroyed
+  };
+
 protected:
   std::string name;
-  Entity     *owner  = nullptr;
   bool        active = true;
+
+  virtual void on_init() {};
+  virtual void on_destroy() {};
+  virtual void update(float delta_time) {};
+  virtual void render() {};
+
+  friend class Entity;
+
+private:
+  Entity *owner = nullptr;
+  State   state = State::Uninitialized;
 
 public:
   explicit Component(const std::string& component_name = "component");
-  virtual ~Component() = default;
+  virtual ~Component() {
+    if (state != State::Destroyed) {
+      on_destroy();
+      state = State::Destroyed;
+    }
+  };
 
-  virtual void init() {};
-  virtual void update(float deltaTime);
-  virtual void render();
+  void init() {
+    if (state == State::Uninitialized) {
+      state = State::Initializing;
+      on_init();
+      state = State::Active;
+    }
+  };
+
+  void destroy() {
+    if (state == State::Active) {
+      state = State::Destroying;
+      on_destroy();
+      state = State::Destroyed;
+    }
+  }
 
   void    set_owner(Entity *entity) { owner = entity; }
   Entity *get_owner() const { return owner; }
