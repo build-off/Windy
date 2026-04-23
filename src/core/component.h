4fc6093d84322ce021df983488b6259c3a58f921
SMKM <msantiagovargas2@gmail.com>
2026-04-22 18:36:45 -0600

    component and entity

--- src/core/component.h
+++ src/core/component.h
@@ -1,20 +1,24 @@
 #pragma once
 
+namespace Windy {
+
 class Entity;
 class Component {
- protected:
-  Entity* owner = nullptr;
+protected:
   std::string name;
-  bool active = true;
+  Entity     *owner  = nullptr;
+  bool        active = true;
 
- public:
-  explicit Component(const std::string& component_name = "component")
-      : name{component_name} {};
+public:
+  explicit Component(const std::string& component_name = "component");
   virtual ~Component() = default;
 
   virtual void init() {};
-  virtual void update(float deltaTime) {};
-  virtual void render() {};
-  void set_owner(Entity* entity) { owner = entity; };
-  Entity* get_owner() const { return owner; };
+  virtual void update(float deltaTime);
+  virtual void render();
+
+  void    set_owner(Entity *entity) { owner = entity; }
+  Entity *get_owner() const { return owner; }
 };
+
+} // namespace Windy
--- src/core/entity.h
+++ src/core/entity.h
@@ -1,54 +1,60 @@
 #pragma once
 
+#include "base.h"
 #include "component.h"
 #include <memory>
 #include <string>
 #include <type_traits>
 #include <utility>
 
+namespace Windy {
+
 class Entity {
- private:
-  std::string name;
-  bool active = true;
+private:
+  std::string                             name;
+  bool                                    active = true;
   std::vector<std::unique_ptr<Component>> components;
 
- public:
+public:
   explicit Entity(const std::string& entityName) : name{entityName} {};
   const std::string& get_name() const { return name; }
-  bool is_active() const { return active; }
-  void set_active(bool is_active) { active = is_active; }
+  bool               is_active() const { return active; }
+  void               set_active(bool is_active) { active = is_active; }
 
   void init() {
-    for (auto& component : components) component->init();
+    for (auto& component : components)
+      component->init();
   }
 
   void update(float delta_time) {
     if (!active) return;
-    for (auto& component : components) component->update(delta_time);
+    for (auto& component : components)
+      component->update(delta_time);
   }
   void render() {
     if (!active) return;
-    for (auto& component : components) component->render();
+    for (auto& component : components)
+      component->render();
   }
 
   /// Create and assign, new components to current entity
   template <typename T, typename... Args>
-  T* add_component(Args&&... args) {
+  T *add_component(Args&&...args) {
     static_assert(std::is_base_of<Component, T>::value,
                   "T must derive from component");
-    auto component = std::make_unique<T>(std::forward(args)...);
-    T* component_ptr = component.get();
+    auto component     = CreateScope<T>(std::forward(args)...);
+    T   *component_ptr = component.get();
     component_ptr->set_owner(this);
     components.push_back(std::move(component));
     return component_ptr;
   }
 
   template <typename T>
-  T* get_component() {
+  T *get_component() {
     for (auto& component : components) {
       // the dynamic_cast can be really slow, so change for better solution to
       // returnrning the component
-      if (T* result = dynamic_cast<T*>(component.get())) {
+      if (T *result = dynamic_cast<T *>(component.get())) {
         return result;
       }
     }
@@ -60,7 +66,7 @@ class Entity {
   template <typename T>
   bool remove_component() {
     for (auto it = components.begin(); it != components.end(); it++) {
-      if (dynamic_cast<T*>(it->get())) {
+      if (dynamic_cast<T *>(it->get())) {
         components.erase(it);
         return true;
       }
@@ -68,3 +74,5 @@ class Entity {
     return false;
   }
 };
+
+} // namespace Windy
