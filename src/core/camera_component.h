2c70732e1b6bf5e3cd9f8da605e355d9ae5da7ea
SMKM <msantiagovargas2@gmail.com>
2026-04-23 09:20:02 -0600

    methods for first camera component

--- src/core/camera_component.h
+++ src/core/camera_component.h
@@ -3,7 +3,9 @@
 #include "component.h"
 #include "entity.h"
 #include "transform_component.h"
-#include <glm/ext/matrix_float4x4.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/vector_float3.hpp>
+#include <glm/trigonometric.hpp>
 
 namespace Windy {
 class CameraComponent final : Component {
@@ -29,8 +31,23 @@ public:
   glm::mat4 get_view_matrix() const {
     auto transform = get_owner()->get_component<TransformComponent>();
     if (transform) {
+      glm::vec3 position = transform->get_position();
+      glm::quat rotation = transform->get_rotation();
+
+      glm::vec3 forward = rotation * glm::vec3(0.0f, 0.0f, -1.0f);
+      glm::vec3 up      = rotation * glm::vec3(0.0f, 1.0f, 0.0f);
+      return glm::lookAt(position, position + forward, up);
     }
     return glm::mat4(1.0f);
+  }
+
+  glm::mat4 GetProjectionMatrix() {
+    if (projection_dirty) {
+      projection_matrix = glm::perspective(glm::radians(field_of_view),
+                                           aspect_ratio, near_place, far_plane);
+      projection_dirty  = false;
+    }
+    return projection_matrix;
   }
 };
 } // namespace Windy
