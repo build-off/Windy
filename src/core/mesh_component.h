44ece638d96d41235a625ab9de2d88ed6a800bc1
SMKM <msantiagovargas2@gmail.com>
2026-04-23 19:06:03 -0600

    bounding box

--- src/core/mesh_component.h
+++ src/core/mesh_component.h
@@ -9,19 +9,23 @@
 
 namespace Windy::Core {
 
+struct BoundingBox {};
+
 class MeshComponent final : Component {
 private:
-  Mesh     *mesh     = nullptr;
-  Material *material = nullptr;
+  Mesh*       mesh     = nullptr;
+  Material*   material = nullptr;
+  BoundingBox bounding_box{};
 
 public:
-  MeshComponent(Mesh *mesh_, Material *material_)
-      : mesh{mesh_}, material{material_} {};
+  MeshComponent(Mesh* mesh_, Material* material_)
+      : mesh{mesh_}, material{material_}, bounding_box{} {};
 
-  void      set_mesh(Mesh *m) { mesh = m; }
-  void      set_material(Material *mat) { material = mat; }
-  Mesh     *get_mesh() const { return mesh; }
-  Material *get_material() const { return material; }
+  void        set_mesh(Mesh* m) { mesh = m; }
+  void        set_material(Material* mat) { material = mat; }
+  Mesh*       get_mesh() const { return mesh; }
+  Material*   get_material() const { return material; }
+  BoundingBox get_bounding_box() const { return bounding_box; }
 
   void render() override {
     if (!mesh || !material) return;
@@ -33,7 +37,7 @@ public:
 
     material->bind();
     material->set_uniform("", transform->get_transform_matrix());
-    mesh->render();
+    // mesh->render();
   };
 };
 
